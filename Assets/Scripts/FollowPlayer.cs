ac82f2c2a7883f170cd599b32590882726c38739
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-04 21:03:10 +0300

    +Added camera switcher

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-
     public GameObject player;
-
+    private bool pressedV;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 closeOffset = new Vector3(0, 2, 5/10);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pressedV = false;
     }
 
     // Update is called once per frame
     void LateUpdate()//firstly moves the vehicle then the camera
     {
         //Offset the camera behind the player by adding to the player's position.
-        this.transform.position = player.transform.position + offset;
+        if (Input.GetKeyDown("v"))
+        {
+            pressedV = true;
+        }
+        else if (Input.GetKeyDown("b"))
+        {
+            pressedV = false;
+            Debug.Log("DüzgünGörüþte");
+        }
+        if (pressedV == false)
+        {
+            this.transform.position = player.transform.position + offset;
+            Debug.Log("DüzgünGörüþte");
+        }
+        else if (pressedV == true)
+        {
+            this.transform.position = player.transform.position + closeOffset;
+            this.transform.rotation = player.transform.rotation;
+            Debug.Log("YakýnGörüþte");
+        }
     }
 }
